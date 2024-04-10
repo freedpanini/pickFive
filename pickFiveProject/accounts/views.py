c8683c95b066c3d1452affc9e9fbc88c47fd405d
Jeffrey Pan <pan.je@northeastern.edu>
2024-04-10 13:04:25 -0400

    added function docstrings

--- pickFiveProject/accounts/views.py
+++ pickFiveProject/accounts/views.py
@@ -10,43 +10,81 @@ from django.template.loader import get_template
 from django.template import Context
 from groups.models import *
 
-  
-#################### index####################################### 
+
+#################### index#######################################
 @login_required
 def index(request):
-    available_groups = Group.objects.filter(IsPublic = True, IsActive = True)
-    user_groups = GroupXAccount.objects.filter(Account= request.user).select_related()
+    """
+    Renders the index page with the available groups and user groups.
 
-    context = {'title':'index', 'available_groups': available_groups, 'user_groups':user_groups}
-    return render(request, 'index.html',context)
-  
-########### register here ##################################### 
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered index page.
+    """
+    available_groups = Group.objects.filter(IsPublic=True, IsActive=True)
+    user_groups = GroupXAccount.objects.filter(Account=request.user).select_related()
+
+    context = {
+        "title": "index",
+        "available_groups": available_groups,
+        "user_groups": user_groups,
+    }
+    return render(request, "index.html", context)
+
+
+########### register here #####################################
 def register(request):
-    if request.method == 'POST':
+    """
+    View function for user registration.
+
+    This function handles the user registration process. It validates the registration form,
+    creates a new user account, and redirects the user to the login page upon successful registration.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object.
+
+    """
+    if request.method == "POST":
         form = AccountRegisterForm(request.POST)
         if form.is_valid():
             account = form.save()
-            username = form.cleaned_data.get('username')
-            email = form.cleaned_data.get('email')
-            messages.success(request, f'Your account has been created ! You are now able to log in')
-            return redirect('login')
+            username = form.cleaned_data.get("username")
+            email = form.cleaned_data.get("email")
+            messages.success(
+                request, f"Your account has been created! You are now able to log in."
+            )
+            return redirect("login")
     else:
         form = AccountRegisterForm()
-    return render(request, 'register.html', {'form': form, 'title':'register here'})
-  
-################ login forms################################################### 
+    return render(request, "register.html", {"form": form, "title": "register here"})
+
+
+################ login forms###################################################
 def login(request):
-    if request.method == 'POST':
-  
+    """
+    View function for handling user login.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object.
+
+    """
+    if request.method == "POST":
         # AuthenticationForm_can_also_be_used__
-  
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username = username, password = password)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             form = login_auth(request, user)
-            return redirect('index')
+            return redirect("index")
         else:
-            messages.info(request, f'account done not exit plz sign in')
+            messages.info(request, f"account does not exist, please sign in")
     form = AuthenticationForm()
-    return render(request, 'login.html', {'form':form, 'title':'log in'})
+    return render(request, "login.html", {"form": form, "title": "log in"})
